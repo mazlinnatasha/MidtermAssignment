1605cb7ab07a66d8deefda0fed88be0b34817266
Mazlin Natasha (288460) <132450794+mazlinnatasha@users.noreply.github.com>
2023-06-13 04:47:05 +0800

    Add files via upload

---
+++ user.dart
@@ -0,0 +1,28 @@
+class User{
+  String? id;
+  String? name;
+  String? email;
+  String? password;
+
+  User(
+    {
+      this.id,
+      this.name,
+      this.email,
+      this.password,
+    });
+  User.fromJson(Map<String, dynamic> json){
+    id = json["id"];
+    name = json["name"];
+    email = json["email"];
+    password = json["password"];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['password'] = this.password;
+    return data;
+  }  
+}
